fc549be53aa626e7e3ba5b62f4c92b1444f37929
bryan aviles <easyrun32@gmail.com>
2020-02-25 19:19:45 -0500

    seperate files and added nodejs for stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -2,13 +2,30 @@ import React from "react";
 import StripeCheckout from "react-stripe-checkout";
 import Logo from "../../assets/matrixx4.png";
 import { stripekey } from "../../config/Key";
+import axios from "axios";
+
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = stripekey;
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Succesful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert("succesful payment");
+      })
+      .catch(error => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
   };
 
   return (
